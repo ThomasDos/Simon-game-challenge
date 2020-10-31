149b18506d4c91196551c09e836715a6a96ceb22
ThomasDos <ThomasDos@users.noreply.github.com>
2020-10-31 22:09:40 +0100

    cleaning and new version of the repo

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ let restarter = true;
 let counterClick = -1;
 let Highscore = 0;
 
+function nextSequence() {
+  level++;
+  counterClick = -1;
+  let randomNumber = Math.floor(Math.random() * 4);
+
+  gamePattern.push(buttonColours[randomNumber]);
+
+  for (let i = 0; i < gamePattern.length; i++) {
+    setTimeout(makeSound, 600 * (i + 1), gamePattern[i]);
+    setTimeout(animatePress, 600 * (i + 1), gamePattern[i]);
+  }
+
+  if (level > Highscore) {
+    Highscore = level;
+  }
+}
+
 function makeSound(value) {
   let audio;
   switch (value) {
@@ -29,23 +46,6 @@ function makeSound(value) {
       break;
   }
   audio.play();
-}
-
-function nextSequence() {
-  level++;
-  counterClick = -1;
-  let randomNumber = Math.floor(Math.random() * 4);
-
-  gamePattern.push(buttonColours[randomNumber]);
-
-  for (let i = 0; i < gamePattern.length; i++) {
-    setTimeout(makeSound, 600 * (i + 1), gamePattern[i]);
-    setTimeout(animatePress, 600 * (i + 1), gamePattern[i]);
-  }
-
-  if (level > Highscore) {
-    Highscore = level;
-  }
 }
 
 function animatePress(currentColor) {
@@ -79,7 +79,7 @@ $(".btn").click(function () {
     let loser = new Audio("./sounds/wrong.mp3");
     loser.play();
     $("#level-title").html(
-      "loser, press any key to reset, Level " + level
+      "loser cunt, press any key to reset, Level " + level
     );
     restarter = true;
   }
